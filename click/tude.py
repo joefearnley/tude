2e76656e1e2b6b5c4900c711d27366d613427211
Joe Fearnley <joe.fearnley@gmail.com>
2022-11-18 22:38:26 -0500

    Remove the argument for all

--- click/tude.py
+++ click/tude.py
@@ -1,10 +1,12 @@
 import click
 
 @click.command()
-@click.option('--all', default=1, help='list all items')
-def list():
-    click.echo('listing....')
+@click.argument('arg')
+def list(arg):
 
+    print(arg)
+
+    click.echo('listing....')
 
 if __name__ == '__main__':
     list()
